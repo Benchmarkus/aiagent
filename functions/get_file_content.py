80db9309b1a20182349d5f9ab1fbbabddcff9bb8
Benchmarkus <toyryla.markus@gmail.com>
2025-07-19 17:26:00 +0300

    file content reading

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+from config import *
+
+def get_file_content(working_directory, filepath):
+
+    combined_path = os.path.join(working_directory, filepath)
+    combined_path_abs = os.path.abspath(combined_path)
+
+    working_directory_abs = os.path.abspath(working_directory)
+
+    if not combined_path_abs.startswith(working_directory_abs):
+        return f'Error: Cannot read "{filepath}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(combined_path):
+        return f'Error: File not found or is not a regular file: "{filepath}"'
+    
+    with open(combined_path, "r") as file:
+        
+        content = file.read()
+        
+        if len(content) > MAX_CHAR:
+            over_max_char = f'[...File "{filepath}" truncated at {MAX_CHAR} characters]'
+            result = content[:MAX_CHAR] + "\n" + over_max_char
+        else: 
+            result = content
+
+    return result
