4718f25da2ecdb42d053fee1ffccd5f86f81a145
Benchmarkus <toyryla.markus@gmail.com>
2025-07-19 12:37:39 +0300

    1st test

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+response_object = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+
+response_text = response_object.text
+prompt_tokens = response_object.usage_metadata.prompt_token_count
+response_tokens = response_object.usage_metadata.candidates_token_count
+
+print(response_text)
+print("Prompt tokens:", prompt_tokens)
+print("Response tokens:", response_tokens)
 
 
 if __name__ == "__main__":
-    main()
+    pass
