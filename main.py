5bdc0ddfda8a2f4e6fc9960d94f78fcf83a9b755
Benchmarkus <toyryla.markus@gmail.com>
2025-07-19 12:53:18 +0300

    sys.argv as prompt input

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response_object = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    try:
+        prompt = sys.argv[1]
+    except IndexError:
+        print("ERROR: No prompt inserted")
+        sys.exit(1)
 
-response_text = response_object.text
-prompt_tokens = response_object.usage_metadata.prompt_token_count
-response_tokens = response_object.usage_metadata.candidates_token_count
+    response_object = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
-print(response_text)
-print("Prompt tokens:", prompt_tokens)
-print("Response tokens:", response_tokens)
+    response_text = response_object.text
+    prompt_tokens = response_object.usage_metadata.prompt_token_count
+    response_tokens = response_object.usage_metadata.candidates_token_count
+
+    print(response_text)
+    print("Prompt tokens:", prompt_tokens)
+    print("Response tokens:", response_tokens)
 
 
 if __name__ == "__main__":
-    pass
+    main()
